0141ac316d217b370d14d3222c4d062d876300bf
Liviu Tudor <enzime4u@yahoo.com>
2019-01-19 21:00:47 +0200

    first todo app with react

--- src/App.js
+++ src/App.js
@@ -1,28 +1,80 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+function TodoItem({todo, index, handleDelete}) {
+  const liClassName = (index + 1) % 3 === 0 ? 'todo green' : 'todo';
+  return (
+       <li className={liClassName}>
+         <span>{todo}</span>
+         <button
+           className="delButton"
+           onClick={() => handleDelete(index)}>x
+          </button>
+      </li>
+    
+  );
+ }
+ 
+ class App extends React.Component {
+  constructor(props) {
+   super(props);
+ 
+   this.state = {
+    input: "",
+    todos: []
+   };
+   this.handleInputChange = this.handleInputChange.bind(this);
+   this.handleSubmit = this.handleSubmit.bind(this);
+   this.handleDelete = this.handleDelete.bind(this);
   }
-}
-
+ 
+  handleDelete(index) {
+   this.setState(prevState => ({
+    todos: prevState.todos.filter((todo, i) => {
+     return i !== index;
+    })
+   }));
+  }
+ 
+  handleInputChange(e) {
+   this.setState({
+    input: e.target.value
+   });
+  }
+ 
+  handleSubmit(e) {
+   e.preventDefault();
+   this.setState(prevState => ({
+    todos: prevState.todos.concat(prevState.input),
+    input: ""
+   }));
+  }
+ 
+  render() {
+   return (
+    <div className="container">
+     <form onSubmit={this.handleSubmit}>
+      <input
+       type="text"
+       value={this.state.input}
+       placeholder="add an item"
+       onChange={this.handleInputChange}
+      />
+      <button className="submit_btn">ADD</button>
+     </form>
+     <ul className="todosContainer">
+      {this.state.todos.map((todo, index) => {
+       return (
+        <TodoItem todo={todo} handleDelete={this.handleDelete} index={index} />
+       );
+      })}
+     </ul>
+    </div>
+   );
+  }
+ }
+ 
+ ReactDOM.render(<App />, document.getElementById("root"));
+ 
 export default App;
